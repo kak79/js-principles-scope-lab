4c1aaa618060dd74682a8f546e2ff5d504aa8413
Kimberly Kohel-Hayes <kohelka@gmail.com>
2021-03-22 17:17:36 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Write your solution in this file!
+
+var customerName = 'bob'
+
+function upperCaseCustomerName(customerName) {
+  customerName = customerName.toUpperCase()
+  print(customerName)
+}
+
+let bestCustomer = 'bob'
+
+function setBestCustomer(bestCustomer) {
+  bestCustomer = 'not bob'
+}
+
+function overwriteBestCustomer(bestCustomer) {
+  bestCustomer = 'maybe bob'
+}
+
+const leastFavoriteCustomer = 'Bob Bartleby'
+
+function changeLeastFavoriteCustomer(leastFavoriteCustomer) {
+  leastFavoriteCustomer = 'definitely bob'
+}
+
+
+
